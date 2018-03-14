d367b765babf00a4c0f79fdc9b2e7ff9b4aaa4d4
Tyler Hancock <darklime@live.ca>
2018-03-13 21:44:49 -0600

    Added some basic code for pulling data from CoinMarketCap.

---
+++ src/main/java/net/foldingcoin/fldcbot/util/CoinInfo.java
@@ -0,0 +1,161 @@
+package net.foldingcoin.fldcbot.util;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class CoinInfo {
+
+    @Expose
+    @SerializedName("id")
+    private String id;
+
+    @Expose
+    @SerializedName("name")
+    private String name;
+
+    @Expose
+    @SerializedName("symbol")
+    private String symbol;
+
+    @Expose
+    @SerializedName("price_usd")
+    private String priceUsd;
+
+    @Expose
+    @SerializedName("price_btc")
+    private String priceBtc;
+
+    @Expose
+    @SerializedName("market_cap_usd")
+    private String marketCapUsd;
+
+    @Expose
+    @SerializedName("available_supply")
+    private String availableSupply;
+
+    @Expose
+    @SerializedName("total_supply")
+    private String totalSupply;
+
+    @Expose
+    @SerializedName("percent_change_1h")
+    private String percentChange1h;
+
+    @Expose
+    @SerializedName("percent_change_24h")
+    private String percentChange24h;
+
+    @Expose
+    @SerializedName("percent_change_7d")
+    private String percentChange7d;
+
+    public String getId () {
+
+        return this.id;
+    }
+
+    public void setId (String id) {
+
+        this.id = id;
+    }
+
+    public String getName () {
+
+        return this.name;
+    }
+
+    public void setName (String name) {
+
+        this.name = name;
+    }
+
+    public String getSymbol () {
+
+        return this.symbol;
+    }
+
+    public void setSymbol (String symbol) {
+
+        this.symbol = symbol;
+    }
+
+    public String getPriceUsd () {
+
+        return this.priceUsd;
+    }
+
+    public void setPriceUsd (String priceUsd) {
+
+        this.priceUsd = priceUsd;
+    }
+
+    public String getPriceBtc () {
+
+        return this.priceBtc;
+    }
+
+    public void setPriceBtc (String priceBtc) {
+
+        this.priceBtc = priceBtc;
+    }
+
+    public String getMarketCapUsd () {
+
+        return this.marketCapUsd;
+    }
+
+    public void setMarketCapUsd (String marketCapUsd) {
+
+        this.marketCapUsd = marketCapUsd;
+    }
+
+    public String getAvailableSupply () {
+
+        return this.availableSupply;
+    }
+
+    public void setAvailableSupply (String availableSupply) {
+
+        this.availableSupply = availableSupply;
+    }
+
+    public String getTotalSupply () {
+
+        return this.totalSupply;
+    }
+
+    public void setTotalSupply (String totalSupply) {
+
+        this.totalSupply = totalSupply;
+    }
+
+    public String getPercentChange1h () {
+
+        return this.percentChange1h;
+    }
+
+    public void setPercentChange1h (String percentChange1h) {
+
+        this.percentChange1h = percentChange1h;
+    }
+
+    public String getPercentChange24h () {
+
+        return this.percentChange24h;
+    }
+
+    public void setPercentChange24h (String percentChange24h) {
+
+        this.percentChange24h = percentChange24h;
+    }
+
+    public String getPercentChange7d () {
+
+        return this.percentChange7d;
+    }
+
+    public void setPercentChange7d (String percentChange7d) {
+
+        this.percentChange7d = percentChange7d;
+    }
+}
---
+++ src/main/java/net/foldingcoin/fldcbot/util/CoinMarketCap.java
@@ -0,0 +1,93 @@
+package net.foldingcoin.fldcbot.util;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+
+import net.darkhax.botbase.BotBase;
+import net.foldingcoin.fldcbot.BotLauncher;
+
+public class CoinMarketCap {
+
+    private static final Gson GSON = new GsonBuilder().create();
+    private static final File DIR_INFO = new File(BotLauncher.DATA_DIR, "coin_info");
+    private static final String COIN_FILE_NAME = "%s_to_%s_%d.json";
+    private static final String URL_COIN_INFO = "https://api.coinmarketcap.com/v1/ticker/%s/?convert=%s";
+    private static final String ID_FLDC = "foldingcoin";
+    private static final String ID_CURE = "curecoin";
+    private static final String ID_USD = "USD";
+
+    /**
+     * Downloads the current info for CureCoin and returns an object containing the current
+     * info.
+     *
+     * @param bot Instance of the bot to download the files.
+     * @return The info for CureCoin at the current moment in time.
+     */
+    public static CoinInfo getCure (BotBase bot) {
+
+        return getCoinInfo(bot, ID_CURE, ID_USD);
+    }
+
+    /**
+     * Downloads the current info for FoldingCoin and returns an object containing the current
+     * info.
+     *
+     * @param bot Instance of the bot to download the files.
+     * @return The info for FoldingCoin at the current moment in time.
+     */
+    public static CoinInfo getFldc (BotBase bot) {
+
+        return getCoinInfo(bot, ID_FLDC, ID_USD);
+    }
+
+    /**
+     * Grabs coin info from CoinMarket cap, saves it to a file, and returns an object
+     * containing the current info.
+     *
+     * @param bot Instance of the bot to download the file.
+     * @param coin The id of the coin you want to get info on.
+     * @param convert The currency to convert to.
+     * @return The current coin info.
+     */
+    // TODO This currently downloads the data every time it is called. This is because we want
+    // the latest data and it is impossible for this method to be fired through normal use at a
+    // rate faster than CoinMarketCap permits. This may be changed to run automatically every
+    // few minutes however performance differences would be relatively insignificant.
+    public static CoinInfo getCoinInfo (BotBase bot, String coin, String convert) {
+
+        // Get the url for the json info
+        final String coinUrl = String.format(URL_COIN_INFO, coin, convert);
+
+        // Get the name of the file to save to
+        final String fileName = String.format(COIN_FILE_NAME, coin, convert, System.currentTimeMillis());
+
+        final File saveDir = new File(DIR_INFO, coin);
+
+        // Downloads the json data and saves it to a file.
+        final File downloadFile = bot.downloadFile(coinUrl, saveDir, fileName);
+
+        try (final JsonReader jsonReader = new JsonReader(new FileReader(downloadFile))) {
+
+            // Construct CoinInfo array from the json data.
+            final CoinInfo[] info = GSON.fromJson(jsonReader, CoinInfo[].class);
+
+            // Data is recieved as an array, but will only have one element.
+            if (info != null && info.length > 0) {
+
+                return info[0];
+            }
+        }
+
+        catch (final IOException e) {
+
+            BotLauncher.LOG.trace("Could not download coin info!", e);
+        }
+
+        return null;
+    }
+}

f79801ed9ebaa1c6e437fddd16474bc859d10409
Tyler Hancock <darklime@live.ca>
2018-03-15 15:37:18 -0600

    Added info command for NACL browser folding.

--- src/main/java/net/foldingcoin/fldcbot/FLDCBot.java
+++ src/main/java/net/foldingcoin/fldcbot/FLDCBot.java
@@ -45,6 +45,7 @@ public class FLDCBot extends BotBase {
         handler.registerCommand("wallet", new CommandWallet());
         handler.registerCommand("browser", new CommandInfo("The Folding Browser", "The folding browser is a tool for making the creation and management of folding accounts easier. You can find more info and downloads " + MessageUtils.makeHyperlink("here", "https://github.com/Hou5e/FoldingBrowser/releases") + "."));
         handler.registerCommand("fah", new CommandInfo("Folding @ Home", "The Stanford Folding At Home client allows users to contribute their idle computation power to medical research. You can find more info and downloads " + MessageUtils.makeHyperlink("here", "http://folding.stanford.edu") + "."));
+        handler.registerCommand("nacl", new CommandInfo("Web Folding Client", "The web client allows you to fold from your internet browser. While the folding browser will not earn as many points, it is faster at completing work units. You can try it out " + MessageUtils.makeHyperlink("here", "http://fahwebx.stanford.edu/nacl/") + "."));
     }
 
     @Override

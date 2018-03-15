80066c1a34db781ded27164168a7d0a6d101cf89
Jared <jaredlll08@gmail.com>
2018-03-15 15:43:34 +0200

    Made the lookup command list multiple users (still needs testing

--- src/main/java/net/foldingcoin/fldcbot/commands/CommandLookup.java
+++ src/main/java/net/foldingcoin/fldcbot/commands/CommandLookup.java
@@ -3,45 +3,67 @@ package net.foldingcoin.fldcbot.commands;
 import net.darkhax.botbase.BotBase;
 import net.darkhax.botbase.commands.Command;
 import net.darkhax.botbase.utils.MessageUtils;
-import net.foldingcoin.fldcbot.util.fldc.FLDCStats;
-import net.foldingcoin.fldcbot.util.fldc.FLDCUser;
-import sx.blah.discord.handle.obj.IChannel;
-import sx.blah.discord.handle.obj.IMessage;
+import net.foldingcoin.fldcbot.util.fldc.*;
+import sx.blah.discord.handle.obj.*;
 import sx.blah.discord.util.EmbedBuilder;
+
+import java.util.*;
 
 /**
  * This command allows users to look up info about the future distributions payout amount.
  */
 public class CommandLookup implements Command {
-
+    
     @Override
-    public void processCommand (BotBase bot, IChannel channel, IMessage message, String[] params) {
-
+    public void processCommand(BotBase bot, IChannel channel, IMessage message, String[] params) {
+        
         // This command requires at least one parameter.
-        if (params.length >= 1) {
+        if(params.length >= 1) {
             final String key = params[0];
-            final EmbedBuilder embed = new EmbedBuilder();
-            final FLDCUser user = FLDCStats.getFutureUser(key);
-            if (user != null) {
-                String fahUsername = String.format("%s_%s_%s", user.getName(), user.getToken(), user.getAddress());
-                embed.withTitle(fahUsername);
-                embed.appendField("Team Rank", user.getId() + "", true);
-                embed.appendField("Token", user.getToken() + "", true);
-                embed.appendField("Unpaid FLDC", String.format("%.8f", (double) FLDCStats.getDifferenceUser(key).getNewCredit() / FLDCStats.differencePoints * 7750000) + "", true);
-                embed.appendField("Address", MessageUtils.makeHyperlink(user.getAddress(),"http://fah-web.stanford.edu/cgi-bin/main.py?qtype=userpage&username="+fahUsername), false);
-
-                embed.withColor(message.getAuthor().getColorForGuild(channel.getGuild()));
-                channel.sendMessage(embed.build());
-            }
-            else {
-                channel.sendMessage("No user found from key: \"" + key + "\"");
+            final List<EmbedBuilder> embeds = new LinkedList<>();
+            EmbedBuilder embed = new EmbedBuilder();
+            embeds.add(embed);
+            final List<FLDCUser> users = FLDCStats.getFutureUsers(key);
+            
+            if(!users.isEmpty()) {
+                
+                boolean multiple = users.size() > 1;
+                for(FLDCUser user : users) {
+                    String fahUsername = String.format("%s_%s_%s", user.getName(), user.getToken(), user.getAddress());
+                    if(embed.getFieldCount() == 0 || embed.getFieldCount() >= 19) {
+        
+                        if(embed.getFieldCount() > 0) {
+                            embeds.add(embed);
+                            embed = new EmbedBuilder();
+                        }
+                        if(!multiple) {
+                            embed.withTitle(fahUsername);
+                        } else {
+                            embed.withTitle(key);
+                        }
+                        embed.withColor(message.getAuthor().getColorForGuild(channel.getGuild()));
+                    }
+                    
+                    embed.appendField("Team Rank", user.getId() + "", true);
+                    embed.appendField("Token", user.getToken() + "", true);
+                    embed.appendField("Unpaid FLDC", String.format("%.8f", (double) FLDCStats.getDifferenceUser(user.getAddress()).getNewCredit() / FLDCStats.differencePoints * 7750000) + "", true);
+                    embed.appendField("Address", MessageUtils.makeHyperlink(user.getAddress(), "http://fah-web.stanford.edu/cgi-bin/main.py?qtype=userpage&username=" + fahUsername), false);
+                    embed.appendField("F@H Username", fahUsername, false);
+                    
+                }
+                for(EmbedBuilder builder : embeds) {
+                    channel.sendMessage(builder.build());
+                }
+            } else {
+                channel.sendMessage("No users found from key: \"" + key + "\"");
             }
         }
+        
     }
-
+    
     @Override
-    public String getDescription () {
-
+    public String getDescription() {
+        
         return "Displays info about the future distribution amount.";
     }
 }

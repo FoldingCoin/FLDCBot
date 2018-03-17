ac9a4799f182fac6017f85513ec3fe7f3d205073
Tyler Hancock <darklime@live.ca>
2018-03-16 22:02:22 -0600

    Fixed potential leak issue.

--- src/main/java/net/foldingcoin/fldcbot/handler/api/APIHandler.java
+++ src/main/java/net/foldingcoin/fldcbot/handler/api/APIHandler.java
@@ -15,21 +15,25 @@ import net.foldingcoin.fldcbot.util.distribution.DistributionUtils;
 import net.foldingcoin.fldcbot.util.fldc.FLDCStats;
 
 public class APIHandler {
+
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     public static void update () {
 
         if (!BotLauncher.instance.getConfig().getWebDir().isEmpty()) {
-            try {
-                final File webDir = new File(BotLauncher.instance.getConfig().getWebDir(), "fldcppd.json");
-                final BufferedWriter writer = new BufferedWriter(new FileWriter(webDir));
+
+            final File webDir = new File(BotLauncher.instance.getConfig().getWebDir(), "fldcppd.json");
+
+            try (final BufferedWriter writer = new BufferedWriter(new FileWriter(webDir))) {
+
                 final int activeFolders = FLDCStats.activeFolders > 0 ? FLDCStats.activeFolders : 1;
                 final FLDCApi api = new FLDCApi(FLDCStats.getTeamPPD(), FLDCStats.activeFolders, (int) (FLDCStats.getTeamPPD() / activeFolders), FLDCStats.distributionsDifference.size(), CoinInfoHandler.getFLDC(), CoinInfoHandler.getBTC(), DateTimeFormatter.ofPattern("yyyy-MM-dd").format(DistributionUtils.getNextDistribution()), DateTimeFormatter.ofPattern("yyyy-MM-dd").format(DistributionUtils.getLastDistribution()), DistributionUtils.getDaysToNextDistribution());
                 writer.write(GSON.toJson(api, FLDCApi.class));
-                writer.close();
             }
+
             catch (final IOException e) {
-                e.printStackTrace();
+
+                BotLauncher.LOG.trace("Could not update the fldcppd.json file!", e);
             }
         }
     }

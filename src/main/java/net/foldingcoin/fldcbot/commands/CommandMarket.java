98267157fbbf67662132893b9dfa393aaffea8a8
Jared <jaredlll08@gmail.com>
2018-03-17 02:08:54 +0200

    Added the market command

---
+++ src/main/java/net/foldingcoin/fldcbot/commands/CommandMarket.java
@@ -0,0 +1,35 @@
+package net.foldingcoin.fldcbot.commands;
+
+import net.darkhax.botbase.BotBase;
+import net.darkhax.botbase.commands.Command;
+import net.darkhax.botbase.utils.MessageUtils;
+import net.foldingcoin.fldcbot.handler.coininfo.CoinInfoHandler;
+import sx.blah.discord.handle.obj.*;
+import sx.blah.discord.util.EmbedBuilder;
+
+import java.text.NumberFormat;
+
+/**
+ * This command allows users to look up info on the FLDC market
+ */
+public class CommandMarket implements Command {
+    
+    @Override
+    public void processCommand(BotBase bot, IChannel channel, IMessage message, String[] params) {
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.withColor(Double.parseDouble(CoinInfoHandler.getFLDC().getPercentChange1h()) > 0 ? 0x00FFFF : 0xFF0000);
+        embed.withTitle(CoinInfoHandler.getFLDC().getName());
+        embed.appendField("Symbol", MessageUtils.makeHyperlink(CoinInfoHandler.getFLDC().getSymbol(), "https://coinmarketcap.com/?utm_medium=widget&utm_campaign=cmcwidget&utm_source=coinmarketcap.com&utm_content=foldingcoin"), true);
+        embed.appendField("MarketCap (USD)", "$" + NumberFormat.getInstance().format(Double.parseDouble(CoinInfoHandler.getFLDC().getMarketCapUsd())), false);
+        embed.appendField("Price (USD)", "$" + CoinInfoHandler.getFLDC().getPriceUsd(), false);
+        embed.appendField("Available Supply", NumberFormat.getInstance().format(Double.parseDouble(CoinInfoHandler.getFLDC().getAvailableSupply())), false);
+        embed.withFooterText("Powered by CoinMarketCap");
+        bot.sendMessage(channel, embed.build());
+    }
+    
+    @Override
+    public String getDescription() {
+        
+        return "Displays info about the FLDC market.";
+    }
+}

2d9f09ded884c0cc578c0d36f16686b2bb93a54a
Jared <jaredlll08@gmail.com>
2018-06-01 16:12:09 +0200

    Fixed the lookup command on distribution days

--- src/main/java/net/foldingcoin/fldcbot/commands/CommandLookup.java
+++ src/main/java/net/foldingcoin/fldcbot/commands/CommandLookup.java
@@ -4,9 +4,7 @@ import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.List;
-
 import net.darkhax.botbase.BotBase;
 import net.darkhax.botbase.commands.Command;
 import net.darkhax.botbase.utils.MessageUtils;
@@ -21,16 +19,16 @@ import sx.blah.discord.util.EmbedBuilder;
  * This command allows users to look up info about the future distributions payout amount.
  */
 public class CommandLookup implements Command {
-
+    
     @Override
     public void processCommand (BotBase bot, IChannel channel, IMessage message, String[] params) {
-
+        
         // This command requires at least one parameter.
         if (params.length >= 1) {
             final String key = params[0];
             final EmbedBuilder embed = new EmbedBuilder();
             final List<FLDCUser> users = FLDCStats.getDifferenceUsers(key);
-
+            
             if (users.isEmpty()) {
                 bot.sendMessage(channel, "No information found for: " + MessageUtils.quote(key) + "!");
             }
@@ -43,21 +41,25 @@ public class CommandLookup implements Command {
                 embed.withTitle(fahUsername);
                 embed.appendField("Team Rank", user.getId() + "", true);
                 embed.appendField("Token", user.getToken() + "", true);
-                embed.appendField("Unpaid FLDC", String.format("%.8f", (double) (user.getNewCredit()+0f) / FLDCStats.getNewPoints() * ((DistributionUtils.getDaysSinceLastDistribution()+1)*250000)) + "", true);
+                
+                double newFLDC = (double) (user.getNewCredit() + 0f) / FLDCStats.getNewPoints() * ((DistributionUtils.getDaysSinceLastDistribution() + 1) * 250000);
+                //On the first day of the month, there are no new points
+                if (FLDCStats.getNewPoints() == 0) {
+                    newFLDC = 0;
+                }
+                embed.appendField("Unpaid FLDC", String.format("%.8f", newFLDC) + "", true);
                 embed.appendField("Address", MessageUtils.makeHyperlink(user.getAddress(), "http://fah-web.stanford.edu/cgi-bin/main.py?qtype=userpage&username=" + fahUsername), false);
-//                http://foldingcoin.xyz/?token=FLDC&total=7750000&start=2018-03-01&end=2018-04-01
-    
                 final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
                 ZoneId zoneID = ZoneId.of("America/Los_Angeles");
-                embed.appendField("Stats", MessageUtils.makeHyperlink("Stats", String.format("http://foldingcoin.xyz/?token=FLDC&total=%s&start=%s&end=%s",((DistributionUtils.getDaysSinceLastDistribution()+1)*250000), dateFormat.format(DistributionUtils.getFirstDay(YearMonth.of(LocalDate.now(zoneID).getYear(), LocalDate.now(zoneID).getMonth()))), dateFormat.format(LocalDate.now(zoneID)))), false);
+                embed.appendField("Stats", MessageUtils.makeHyperlink("Stats", String.format("http://foldingcoin.xyz/?token=FLDC&total=%s&start=%s&end=%s", ((DistributionUtils.getDaysSinceLastDistribution() + 1) * 250000), dateFormat.format(DistributionUtils.getFirstDay(YearMonth.of(LocalDate.now(zoneID).getYear(), LocalDate.now(zoneID).getMonth()))), dateFormat.format(LocalDate.now(zoneID)))), false);
                 bot.sendMessage(channel, embed.build());
             }
         }
     }
-
+    
     @Override
     public String getDescription () {
-
+        
         return "Displays info about the future distribution amount.";
     }
 }
